c6ee5b7f2e2a2d9adfb642c7b116ea9ebdeaa7c3
Aishaakin <muakin12@gmail.com>
2025-08-17 20:48:08 +0100

    done with episode 59

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,18 @@
 export default function FoodDetails ({ foodId }) {
-    return <div> THE FOOD MENU {foodId} </div>
+    const [food,setFood] = useState({});
+    const URL = `https://api.spoonacular.com/recipes/${foodId}/information`
+    const API_KEY = "2cf5e88a0bf14f87909d5fdb3ad2ac27";
+    useEffect(() => {
+        async function fetchFood() {
+        const response = await fetch(`${URL}?apiKey=${API_KEY}`)
+        const data = await response.json();
+        console.log(data);    
+        setFood(data);
+    }
+        fetchFood();
+    }, []);
+    return <div> 
+        THE FOOD MENU {foodId}
+         {food.title}
+     </div>
 }
